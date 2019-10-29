f006fb0208f5cfb4b2a178b182ba35c4f9526a4b
tututou <ba754309554@126.com>
2019-10-29 09:15:47 +0800

    Update img_extract.py

--- doc2mysql/img_extract.py
+++ doc2mysql/img_extract.py
@@ -2,7 +2,7 @@ import docx
 import os
 from PIL import Image
 import datetime
-from docx.parts.image import ImagePart
+
 
 
 
@@ -11,17 +11,7 @@ from docx.parts.image import ImagePart
 def B27_img_extract(media_B27,ImageName,RPmtime_urltime,FiledocxPathName):
     doc = docx.Document(FiledocxPathName)
     ImgSavePathlist = []
-    rIdlist = []
-    docpic = doc.part.related_parts
-    for i in docpic:
-        # 判断docpic[i]是Imagepart 类，首先导入 Imagepart 类，一次类推，以后判断类型，不光可以判断int ,str dict， list ，也可判断自定义类
-        if isinstance(docpic[i], ImagePart):
-            rIdlist.append(i)
-        else:
-            pass
-    print(rIdlist)
-    rIdlist.sort()
-    print(rIdlist)
+    rIdlist = ['rId7']
     for i in rIdlist:
         imgSavePath = media_B27 + '\\' + ImageName + RPmtime_urltime + '_' + i + '.jpg'
         # imgSavePath.encode('utf-8')
